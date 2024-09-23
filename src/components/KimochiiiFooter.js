95a5c606d5b1884991cf541c07592c98485bf88d
QUANON <quanon86@gmail.com>
2024-09-24 05:20:26 +0900

    Break the text in the footer

--- src/components/KimochiiiFooter.js
+++ src/components/KimochiiiFooter.js
@@ -4,8 +4,10 @@ import Typography from '@mui/material/Typography';
 
 const KimochiiiFooter = () => (
   <Box display="flex" alignItems="center" justifyContent="center">
-    <Typography sx={{ fontSize: '0.55rem' }} color="rgba(0, 0, 0, 0.6)" gutterBottom>
-      This app is based on <a href="https://www.youtube.com/watch?v=ts6CJosyLHk" target="_blank" rel="noopener noreferrer">this video</a>. Please contact <a href="https://x.com/quanon_jp" target="_blank" rel="noopener noreferrer">@quanon_jp</a> if you have any questions.
+    <Typography sx={{ fontSize: '0.6rem' }} color="rgba(0, 0, 0, 0.6)" align="center" gutterBottom>
+      This app is based on <a href="https://www.youtube.com/watch?v=ts6CJosyLHk" target="_blank" rel="noopener noreferrer">this video</a>.
+      <br />
+      Please contact <a href="https://x.com/quanon_jp" target="_blank" rel="noopener noreferrer">@quanon_jp</a> if you have any questions.
     </Typography>
   </Box>
 );
