12ed5ca6c2c1b59a8e4f9fcb7e7919d0493fc669
QUANON <quanon86@gmail.com>
2024-09-23 20:25:14 +0900

    Open the link in a new tab

--- src/components/KimochiiiFooter.js
+++ src/components/KimochiiiFooter.js
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography';
 const KimochiiiFooter = () => (
     <Box display="flex" alignItems="center" justifyContent="center">
       <Typography sx={{ fontSize: '0.55rem' }} color="rgba(0, 0, 0, 0.6)" gutterBottom>
-        This app is based on <a href="https://www.youtube.com/watch?v=ts6CJosyLHk">this video</a>. Please contact <a href="https://x.com/quanon_jp">@quanon_jp</a> if you have any questions.
+        This app is based on <a href="https://www.youtube.com/watch?v=ts6CJosyLHk" target="_blank" rel="noopener noreferrer">this video</a>. Please contact <a href="https://x.com/quanon_jp" target="_blank" rel="noopener noreferrer">@quanon_jp</a> if you have any questions.
       </Typography>
     </Box>
 );
